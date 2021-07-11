a0dc851b3e901af15399c241eb4c2f044955854a
suhasagg <suhasagg@gmail.com>
2021-07-12 03:09:58 +0530

    .

---
+++ labeloutput/header.php
@@ -0,0 +1,27 @@
+<div id="header">
+			<div id="title">
+				<div id="left">
+					<center class="dg"> </center>
+					<center class="lg"> </center>
+					<center class="lb">L</center>
+					<center class="dg">A</center>
+					<center class="lg">B</center>
+                                        <center class="lb">E</center>
+					<center class="dg">L</center>
+					<center class="lg"> </center>
+                                        <center class="lb"> </center>
+				</div>
+				<div id="right">
+                                        <center class="lo"> </center>  
+					<center class="lp"></center>
+					<center class="lr">O</center>
+					<center class="ly">U</center>
+					<center class="lp">T</center>
+					<center class="lr">P</center>
+					<center class="ly">U</center>
+                                        <center class="lp">T</center>
+					<center class="lr"> </center>
+					
+				</div>
+			</div>
+</div>
